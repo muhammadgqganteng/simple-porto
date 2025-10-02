86d94ba27859201bb84e30c456e3315c69434ce3
Muhammad <muhammad.160209@gmail.com>
2025-10-02 23:59:43 +0700

    menambahkan Project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./pages/**/*.html", "./projects/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
